d29dfde7a38c7ba7f2476651e326a318eb047f10
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-11-30 22:29:43 +0100

    Improve performance of face detection

--- app/src/main/java/pl/edu/agh/sm/eyetracking/EyeTrackingProcessor.java
+++ app/src/main/java/pl/edu/agh/sm/eyetracking/EyeTrackingProcessor.java
@@ -12,60 +12,89 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 
-
 public class EyeTrackingProcessor implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private Mat outputRGBA;
-    private CascadeClassifier faceDetector;
+    private Mat outputImage;
+    private Mat scaledGrayImage;
+    private Size scaledSize;
+
+    private int imageWidth;
+    private int imageHeight;
+
+    private final CascadeClassifier faceDetector;
+
+
+    private final int SCALE = 8;
+
 
     EyeTrackingProcessor(CascadeClassifier classifier) {
-        super();
         faceDetector = classifier;
+    }
+
+    private int getScaledWidth() {
+        return imageWidth / SCALE;
+    }
+
+    private int getScaledHeight() {
+        return imageHeight / SCALE;
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        outputRGBA = new Mat(height, width, CvType.CV_8UC4);
+        this.imageWidth = width;
+        this.imageHeight = height;
+
+//        outputImage = new Mat(height, width, CvType.CV_8UC4);
+        scaledGrayImage = new Mat(getScaledHeight(), getScaledWidth(), CvType.CV_8UC1);
+        scaledSize = new Size(getScaledWidth(), getScaledHeight());
     }
 
     @Override
     public void onCameraViewStopped() {
-        outputRGBA.release();
+        outputImage.release();
+        scaledGrayImage.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
         process(cvCameraViewFrame);
-        return outputRGBA;
+        return outputImage;
     }
 
     private void process(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
-        Mat inputMat = cvCameraViewFrame.rgba();
-//        Imgproc.cvtColor(inputMat, inputMat, Imgproc.COLOR_RGB2BGRA);
+        Mat inputImage = cvCameraViewFrame.rgba();
 
-        Mat progressMat = inputMat.clone();
-//        Imgproc.Canny(inputMat, progressMat, 80, 90);
-//        Imgproc.cvtColor(inputMat, progressMat, Imgproc.COLOR_RGBA2GRAY);
-
-        // face detection
+        Imgproc.resize(cvCameraViewFrame.gray(), scaledGrayImage, scaledSize);
+        // --- face detection
         MatOfRect faces = new MatOfRect();
-        faceDetector.detectMultiScale(progressMat, faces);
-//        faceDetector.detectMultiScale(progressMat, faces, 1.1,2,2,
-//                new Size(10,10),new Size(400,400));
+        faceDetector.detectMultiScale(
+                scaledGrayImage,
+                faces,
+                1.1,
+                2,
+                2,
+                new Size(getScaledHeight() >> 2, getScaledHeight() >> 2),
+                new Size(getScaledHeight(), getScaledHeight())
+        );
+
+//        Mat progressMat = new Mat((int) scaledSize.height, (int) scaledSize.width, CvType.CV_8UC4);
+//        Imgproc.cvtColor(scaledGrayImage, progressMat, Imgproc.COLOR_GRAY2RGBA);
+//        progressMat.copyTo(inputImage.submat(new Rect(0, 0, (int) scaledSize.width, (int) scaledSize.height)));
+//        progressMat.release();
 
         // draw red rectangles for detected faces
         for (Rect rect : faces.toArray()) {
-            Imgproc.rectangle(progressMat,
-                    new Point(rect.x, rect.y),
-                    new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(255, 0, 0)
+            Imgproc.rectangle(inputImage,
+                    new Point(rect.x * SCALE, rect.y * SCALE),
+                    new Point((rect.x + rect.width) * SCALE, (rect.y + rect.height) * SCALE),
+                    new Scalar(255, 0, 0),
+                    3
             );
         }
 
-        progressMat.copyTo(outputRGBA);
-
-        inputMat.release();
-        progressMat.release();
+//        inputImage.copyTo(outputImage);
+//        inputImage.release();
+        outputImage = inputImage;
     }
 
 }
